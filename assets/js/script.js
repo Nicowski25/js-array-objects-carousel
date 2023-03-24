ab419c6f31ad4a8f87e30855eea2db66444686ab
Nicowski25 <nicola.faedo0610@gmail.com>
2023-03-24 16:36:48 +0100

    class correction, tofix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 //creo l'array delle immagini
-const album = [
+const images = [
     {
         image: 'img/01.webp',
         title: 'Marvel\'s Spiderman Miles Morale',
@@ -23,7 +23,7 @@ const album = [
     }
 ];
 
-//funzione per stampare in dom
+/* //funzione per stampare in dom
 function printToDOM(id, text) {
     document.querySelector(id).innerHTML += text
 }
@@ -36,10 +36,104 @@ console.log(images);
 let i = 0
 while (i < images.length) {
     const carouselImage = `
-    <div class="carousel-inner">
-        <img src="./assets/${images[i]}" class="d-block w-100" alt="">
-    </div>
+    <img src="./assets/${images[i]}" class="w-100" alt="">
     `
     printToDOM('#carousel', carouselImage)
+    if (i == 0) {
+        document.querySelector('img').classList.add('active')
+    }
     i++
+} */
+
+let active_slide = 0
+const slides_dom_element = document.getElementById('slides')
+const thumbs_dom_element = document.querySelector('thumbs')
+const prev_dom_element = document.querySelector('button.prev')
+const next_dom_element = document.querySelector('button.next')
+const slider_dom_element = document.querySelector('slider')
+
+images.forEach((slide, index) => {
+    const slide_markup= `
+    <div class="slide" ${index === active_slide ? 'active' : ''}>
+        <img src="./assets/${slide.image}">
+        <div class="text">
+            <h3>${slide.title}</h3>
+            <p>${slide.text}</p>
+        </div>
+    </div>
+    `
+    slides_dom_element.insertAdjacentElement('beforeend', slide_markup)
+
+    const thumb_markup = `<img src="./assets/${slide.image}" class="${index === active_slide ? 'active' : ''}>`
+    thumbs_dom_element.insertAdjacentElement('beforeend', thumb_markup)
 }
+)
+
+next_dom_element.addEventListener('click', next)
+
+function next() {
+    //incremento il counter per la prossima slide attiva
+    //controlliamo se siamo all'ultimo elemento della array e nel caso settiamo active slide = 0
+    if (active_slide === images.length -1) {
+        active_slide = 0
+    } else {
+        active_slide ++
+    }
+    //selezioniamo slide corrente
+    const current_slide = document.querySelector('.slide.active')
+    //rimuoviamo classe active
+    current_slide.classList.remove('active')
+    //selezioniamo prossima immagine dalla lista
+    const next_slide = document.querySelectorAll('.slide')[active_slide]
+    next_slide.classList.add('active')
+    //seleziono il thumbnail
+    const current_thumb = document.querySelector('.thumb > img.active')
+    current_thumb.classList.remove('active')
+    //selezioniamo il prossimo thumb
+    const next_thumb = document.querySelectorAll('.thumb > img')[active_slide]
+    //aggiungiamo la classe active
+    next_thumb.classList.add('active')
+}
+
+
+prev_dom_element.addEventListener('click', prev)
+
+function prev() {
+    //decrementiamo il counter per la prossima slide attiva
+    //controlliamo se siamo all'primo elemento della array e nel caso settiamo active slide = -1
+    if (active_slide === 0) {
+        active_slide = images.length - 1
+    } else {
+        active_slide --
+    }
+    //selezioniamo slide corrente
+    const current_slide = document.querySelector('.slide.active')
+    //rimuoviamo classe active
+    current_slide.classList.remove('active')
+    //selezioniamo prossima immagine dalla lista
+    const next_slide = document.querySelectorAll('.slide')[active_slide]
+    next_slide.classList.add('active')
+    //seleziono il thumbnail
+    const current_thumb = document.querySelector('.thumb > img.active')
+    current_thumb.classList.remove('active')
+    //selezioniamo il prossimo thumb
+    const next_thumb = document.querySelectorAll('.thumb > img')[active_slide]
+    //aggiungiamo la classe active
+    next_thumb.classList.add('active')
+}
+
+//bonus 2 autoplay
+let autoplay 
+autoplay = setInterval(() => {}, 3000)
+
+
+//bonus 3
+slider_dom_element.addEventListener('mouseenter', function() {
+    clearInterval(autoplay)
+})
+//quando il mouse esce dallo slider
+slider_dom_element.addEventListener('mouseleave', function() {
+    autoplay = setInterval(next, 3000)
+})
+
+
